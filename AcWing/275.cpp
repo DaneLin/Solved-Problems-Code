5acc54d77682d87604630bb17677fcbfe64cdca3
DaneLin <57523864+DaneLin@users.noreply.github.com>
2021-04-23 21:09:17 +0800

    Add O(n^3) solution

--- AcWing/275.cpp
+++ AcWing/275.cpp
@@ -46,3 +46,51 @@ int main()
 
     return 0;
 }
+
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+const int N=60;
+
+int g[N][N];
+int dp[N+N][N][N];
+
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            cin>>g[i][j];
+        }
+    }
+
+    for(int k=2;k<=n+m;k++)
+        for(int i1=1;i1<=n;i1++)
+            for(int i2=1;i2<=n;i2++)
+            {
+                int j1=k-i1,j2=k-i2;
+                if(j1>=1&&j1<=m&&j2>=1&&j2<=m) 
+                {
+                    int t=g[i1][j1];
+                    if(i1!=i2) t+=g[i2][j2];
+                    int &x=dp[k][i1][i2];
+                    x=max(x,dp[k-1][i1-1][i2]+t);
+                    x=max(x,dp[k-1][i1-1][i2-1]+t);
+                    x=max(x,dp[k-1][i1][i2]+t);
+                    x=max(x,dp[k-1][i1][i2-1]+t);
+                }
+            }
+
+    cout<<dp[n+m][n][n];
+
+    return 0;
+}

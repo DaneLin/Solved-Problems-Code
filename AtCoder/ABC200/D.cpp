4198233317ce219051bc60746e4866b26d7b62a9
DaneLin <57523864+DaneLin@users.noreply.github.com>
2021-05-09 01:05:11 +0800

    added solution to problem D

--- AtCoder/ABC200/D.cpp
+++ AtCoder/ABC200/D.cpp
@@ -3,7 +3,47 @@
 using namespace std;
 typedef long long LL;
 
+void print(vector<int> arr){
+    cout<<arr.size();
+    for(auto c:arr){
+        cout<<" "<<c;
+    }
+    cout<<endl;
+}
+
 int main()
 {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++) cin>>a[i];
+
+    int cnt=min(8,n);
+    vector<int> rem[201];
+    for(int i=1;i<1<<cnt;i++){
+        int seg=0;  
+        vector<int> tmp;
+        for(int j=0;j<cnt;j++){
+            if(i&(1<<j)){
+                tmp.push_back(j+1);
+                seg+=a[j];
+                seg%=200;
+            }
+        }
+
+        if(rem[seg].size()!=0){
+            cout<<"Yes"<<endl;
+            print(rem[seg]);
+            print(tmp);
+            return 0;
+        }
+        else
+        {
+            rem[seg]=tmp;
+        }
+    }
+    cout<<"No"<<endl;
+
     return 0;
 }
